d071cba1b4f2e9965a902bd0becbf796e6811f42
Hikaru <80188901@163.com>
2016-05-19 00:28:23 +0800

    create_login_session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,15 @@ class UsersController < ApplicationController
   end
 
   def login
+
+  end
+
+  def create_login_session
+    user = User.find_by_mobile(:mobile)
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+    else
+      redirect_to :login
+    end
   end
 end
