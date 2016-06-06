2691476c483e4612d54def06b55771b03fe2dca8
杨志强 <yangzhiqiang@My-Wifi.local>
2016-06-06 21:56:13 +0800

    最后修改

--- app/controllers/verify_code_controller.rb
+++ app/controllers/verify_code_controller.rb
@@ -5,7 +5,8 @@ class VerifyCodeController < ApplicationController
     verify_code = { code: generate_code, expire_at: Time.now + 2.minutes, auth_state: false }
     if VerifyCodeHelper.send_auth_code_sms mobile, verify_code[:code]
       session[:verify_code] = verify_code
-      render js: "alert(#{session[:verify_code][:code]})"
+      # render js: "alert(#{session[:verify_code][:code]})"
+      puts session[:verify_code][:code]
     else
       render js: "alert('手机发送验证码失败')"
     end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,11 @@
 class WelcomeController < ApplicationController
   layout "public"
   def index
-    @articles = Article.paginate(:page => params[:page], :per_page => 16)
+    @articles = Article.order(created_at: :desc).paginate(:page => params[:page], :per_page => 16)
   end
 
   def classical
-    @classicals = Article.paginate(:page => params[:page], :per_page => 16).where(good: 1)
+    @classicals = Article.order(created_at: :desc).paginate(:page => params[:page], :per_page => 16).where(good: 1)
   end
 
   def about
